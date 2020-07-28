de9b1d18ea848f429e225c81a04ab8b1839c0332
atses <68601197+atses@users.noreply.github.com>
2020-07-28 16:47:58 +0200

    Update Backup_SQL_Full.ps1
    Add english commentary

--- Backup_SQL_Full.ps1
+++ Backup_SQL_Full.ps1
@@ -1,9 +1,11 @@
-﻿#Variables
+#Variables
 $Destination = ""
 $BDD1 = ""
 $BDD2 = ""
 $sqlserver = ""
-#Renomme les sauvegardes précédentes avec la date du jour
+$Days = ""
+
+#Renomme les sauvegardes précédentes avec la date du jour / Rename previous backups adding today's date
 Get-ChildItem $Destination+$BDD1 | ForEach-Object {          
 Rename-Item $_.FullName $BDD1+"_$(Get-Date $_.CreationTime -Format "ddMMyyyy").BAK"
 }
@@ -11,21 +13,21 @@ Get-ChildItem $Destination+$BDD2 | ForEach-Object {
 Rename-Item $_.FullName $BDD2+"_$(Get-Date $_.CreationTime -Format "ddMMyyyy").BAK"
 }
 
-#Récupère la date 7 jours en arrière
-$Date7Jours = (Get-Date).AddDays(-7)
+#Récupère la date X jours en arrière / Get date X days before
+$Versions = (Get-Date).AddDays(-$Days)
 
-#Supprime tous les fichiers .BAK qui ont été créés il y a plus de 7 jours
+#Supprime tous les fichiers .BAK qui ont été créés il y a plus de X jours / Delete all .BAK files older than X days
 Get-ChildItem -path $Destination+'*' -include *.BAK | ForEach-Object {
 
     $DateCreation = Get-Date $_.CreationTime
 
-    if ($DateCreation -lt $Date7Jours)
+    if ($DateCreation -lt $Versions)
     {
         Remove-Item -LiteralPath $_.FullName
     }
 }
 
-#Requete SQL de sauvegarde de toutes les bases qui ne sont pas des bases par défaut (il faut déclarer le chemin dans @path)
+#Requete SQL de sauvegarde de toutes les bases qui ne sont pas des bases par défaut (il faut déclarer le chemin dans @path) / SQL request, backup every databases that are not default databases (you have to declare the @path)
 $sql ="
 DECLARE @name VARCHAR(50) -- database name  
 DECLARE @path VARCHAR(256) -- path for backup files  
@@ -54,10 +56,10 @@ END
 CLOSE db_cursor   
 DEALLOCATE db_cursor 
 "
-#Execute la commande sql en désactivant le timeout
+#Execute la commande sql en désactivant le timeout / Run SQL without timeout
 Invoke-Sqlcmd -ServerInstance $sqlserver -Database msdb -Query $sql -querytimeout 0
 
-#preparation mail
+#Preparation mail / Email setup
 [System.Net.ServicePointManager]::ServerCertificateValidationCallback = { return $true }
 $SMTPServer = ""
 $SMTPPort = ""
@@ -69,17 +71,16 @@ $smtp = New-Object System.Net.Mail.SmtpClient($SMTPServer, $SMTPPort);
 $smtp.EnableSSL = $true
 $smtp.Credentials = New-Object System.Net.NetworkCredential($Username, $Password);
 
-#Check_Save_Oveo
-#Recupere la date du jour
+#Recupere la date du jour / Get today's date
 $date = Get-Date -Format "dd/MM/yyyy"
 
-#Recupere la description des events MSSQLSERVER du jour avec l'id 3041 ou 3014 (Resussite ou echec)
+#Recupere la description des events MSSQLSERVER du jour avec l'id 3041 ou 3014 (Resussite ou echec) / Get MSSQLSERVER event details from today with id 3041 or 3014 (succes or failure)
 $message = Get-EventLog -LogName Application -Source MSSQLSERVER -After $date | Where {($_.EventID -eq 3041 -or $_.EventID -eq 3014)} | Select-Object -Property Message
 
-#Si l'objet "message" contient le nom de la base c'est que la sauvegarde n'a pas fonctionné, déclenche alors l'envoi d'email echec
+#Si l'objet "message" contient le nom de la base c'est que la sauvegarde n'a pas fonctionné, déclenche alors l'envoi d'email echec / If the object "messge" contains the databases's name we can assume the backup didn't work then send a failure email
 if ($message -match $BDD1)
 {
-    #envoi mail echec
+    #Envoi mail echec / Failure email
     $subject = "[ECHEC] Sauvegarde XXXXX a echouee"
 	$message = New-Object System.Net.Mail.MailMessage
 	$message.subject = $subject
@@ -89,10 +90,10 @@ if ($message -match $BDD1)
 	$smtp.send($message)
 }
 
-#Si l'objet "message" ne contient pas le nom de la base c'est que la sauvegarde a pas fonctionné, déclenche alors l'envoi d'email reussite
+#Si l'objet "message" ne contient pas le nom de la base c'est que la sauvegarde a pas fonctionné, déclenche alors l'envoi d'email reussite / If the object "message" doesn't contain the databse's name we can assume the backup worked then send a succes email
 else
 {
-    #envoi mail succes
+    #Envoi mail succes / Success email
     $subject = "Sauvegarde SQL XXXXX realisee avec succes"
 	$message = New-Object System.Net.Mail.MailMessage
 	$message.subject = $subject
@@ -102,14 +103,14 @@ else
 	$smtp.send($message)
 }
 
-#Recupere la date du jour
+#Répeter pour chaque base de données / Repeat for each database
 $date = Get-Date -Format "dd/MM/yyyy"
-#Recupere la description des events MSSQLSERVER du jour avec l'id 3041 ou 3014 (Resussite ou echec)
+
 $message = Get-EventLog -LogName Application -Source MSSQLSERVER -After $date | Where {($_.EventID -eq 3041 -or $_.EventID -eq 3014)} | Select-Object -Property Message
-#Si l'objet "message" contient le nom de la base c'est que la sauvegarde n'a pas fonctionné, déclenche alors l'envoi d'email echec
+
 if ($message -match $BDD2)
 {
-    #envoi mail echec
+
     $subject = "[ECHEC] Sauvegarde XXXXX a echouee"
 	$message = New-Object System.Net.Mail.MailMessage
 	$message.subject = $subject
@@ -119,10 +120,9 @@ if ($message -match $BDD2)
 	$smtp.send($message)
 }
 
-#Si l'objet "message" ne contient pas le nom de la base c'est que la sauvegarde a pas fonctionné, déclenche alors l'envoi d'email reussite
 else
 {
-    #envoi mail succes
+
     $subject = "Sauvegarde XXXXX realisee avec succes"
 	$message = New-Object System.Net.Mail.MailMessage
 	$message.subject = $subject
